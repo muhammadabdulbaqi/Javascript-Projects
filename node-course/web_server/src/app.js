906a3eb1a0c734419807d2f8ffd3ba52297dbd29
Muhammad <159867482+muhammadabdulbaqi@users.noreply.github.com>
2025-06-25 06:58:06 -0700

    Handlebars

--- node-course/web_server/src/app.js
+++ node-course/web_server/src/app.js
@@ -6,7 +6,32 @@ import { fileURLToPath } from 'url';
 const app = express()
 const publicDirectoryPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../public');
 
+app.set('view engine', 'hbs')
+
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Muhammad Abdulbaqi'
+    })  
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Muhammad Abdulbaqi'
+    })
+})
+
+app.get('/help', (req, res) => {
+
+    res.render('help', {
+        title: 'Help',
+        email: 'm.abdulbaqi1702@gmail.com',
+        message: 'We are here to help you with any issues you may have.',
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
