2fed4c4cc5b4a5e0378cafb0cb59bdd959e6a6d8
Muhammad <159867482+muhammadabdulbaqi@users.noreply.github.com>
2025-07-06 04:49:48 -0700

    Testing req w/ query string

--- web_server/src/app.js
+++ web_server/src/app.js
@@ -46,9 +46,28 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        })
+    }
+    console.log(req.query.address)
     res.send({
         forecast: 'Sunny',
         location: 'New York',
+        address: req.query.address
+    })
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search) {
+        return res.send({
+            error: 'You must provide a search term.'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
     })
 })
 
